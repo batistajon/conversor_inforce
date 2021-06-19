18f364c30a6b0d35f36758c34d85db7ba77ea9a7
Jon Batista <batista.jonathas@gmail.com>
2021-06-19 14:33:04 -0300

    Chamada a API externa para teste de cotação. Setando modelo de resposta json

--- app/Http/Controllers/ConversorController.php
+++ app/Http/Controllers/ConversorController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Conversor;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Http;
 
 class ConversorController extends Controller
 {
@@ -14,5 +17,37 @@ class ConversorController extends Controller
         $this->conversor = $conversor;
     }
 
-    
+    /**
+     * Method converter
+     *
+     * @param $base $base [explicite description]
+     * @param $to $to [explicite description]
+     * @param $value $value [explicite description]
+     *
+     * @return JsonResponse
+     */
+    public function converter($base, $to, $value = 1): JsonResponse
+    {
+        try {
+            
+            $query = $base.'_'.$to;
+
+            $response = Http::get(
+                "https://free.currconv.com/api/v7/convert?q={$query}&compact=ultra&apiKey=".env('API_KEY_COTACAO')
+            )[$query];
+
+            $result = $value * $response;
+
+            return response()->json([
+                    'cotacao' => $response,
+                    'resultado' => $result
+                ], 200);
+
+        } catch (\Exception $e) {
+
+            return response()->json([
+                    'erro' => 'Insira um valor para realizar a cotação'
+                ], 400);
+        } 
+    }
 }
