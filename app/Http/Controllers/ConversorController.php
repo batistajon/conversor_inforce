9e7a914786e10e5b69c29073c165163622e02cbe
Jon Batista <batista.jonathas@gmail.com>
2021-06-19 14:19:13 -0300

    Injeção de dependencia da classe Conversor  via construtor no CoversorController

--- app/Http/Controllers/ConversorController.php
+++ app/Http/Controllers/ConversorController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Conversor;
 use Illuminate\Http\Request;
 
 class ConversorController extends Controller
 {
-    //
+    private $conversor;
+
+    public function __construct(Conversor $conversor)
+    {
+        $this->conversor = $conversor;
+    }
 }
